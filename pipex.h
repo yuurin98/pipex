3222b22734372635a1104454d60afa05ebc741a5
yuurin98 <119097638+yuurin98@users.noreply.github.com>
2024-05-20 10:18:17 +0000

    header includes

---
+++ pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 09:31:11 by codespace         #+#    #+#             */
+/*   Updated: 2024/05/20 09:38:30 by codespace        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/wait.h>
+
+#endif
