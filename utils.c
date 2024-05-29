355580809fee47d38aa8ad2ce8fe5dff4e7821b4
yuurin98 <119097638+yuurin98@users.noreply.github.com>
2024-05-29 08:26:18 +0000

    main skeleton done

--- utils.c
+++ utils.c
@@ -1,36 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/20 09:31:08 by codespace         #+#    #+#             */
-/*   Updated: 2024/05/29 08:07:49 by codespace        ###   ########.fr       */
+/*   Created: 2024/05/29 08:00:10 by codespace         #+#    #+#             */
+/*   Updated: 2024/05/29 08:03:02 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	parent(char **argv, int *fd, char **env)
+void	error_exit(char *msg)
 {
-	
-}
-
-int	main(int argc, char **argv, char **env)
-{
-	int		fd[2];
-	pid_t	pid;
-	
-	if (argc != 5)
-		error_exit("./pipex infile cmd cmd outfile\n")；
-	if (pipe(fd) == -1)
-		exit(-1);
-	pid = fork();
-	if (pid == -1)
-		exit(-1);
-	if (!pid)
-		child(argv, fd, env);
-	parent(argv, fd, env);
-	return (0);
+	ft_printf(msg);
+	exit(1);
 }
